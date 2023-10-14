5ef3451a5cc9ae98dacb4e3dc2a43fae9bef9505
Phutawan Palakavong <phutawan.pram@gmail.com>
2023-10-14 20:07:17 +0700

    Fix Style Submit response

--- src/components/UI/molegules/SubmitResponse.jsx
+++ src/components/UI/molegules/SubmitResponse.jsx
@@ -1,7 +1,11 @@
 function SubmitResponse() {
   return (
     <div className="flex flex-col h-full w-full justify-center items-center">
-      <img src="/icon-thank-you.svg" alt="thank you icon" className="" />
+      <img
+        src="/icon-thank-you.svg"
+        alt="thank you icon"
+        className="mt-10 lg:mt-0"
+      />
       <h1 className="text-[#02295A] text-3xl font-[700] m-5">Thank you</h1>
       <p className="text-center">
         Thanks for confirming your subscription! We hope you have fun using our
